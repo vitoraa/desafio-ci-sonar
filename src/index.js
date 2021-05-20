294522874ab57c0c289f457a7ac3c4c927e177af
vitoraa <vitor.lima@outlook.com>
2021-05-20 08:59:30 -0300

    ci:

--- src/index.js
+++ src/index.js
@@ -6,4 +6,12 @@ function multiply(a, b) {
   return a * b;
 }
 
-module.exports = { sum, multiply };
+function divide(a, b) {
+  return a / b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+module.exports = { sum, multiply, divide, subtract };
